6c7162f34372b28534b3fde5cc2b27c2d8f3aa42
szp99 <70907288+szp99@users.noreply.github.com>
2022-06-15 15:21:30 +0200

    Add files via upload

---
+++ src/polyfills.ts
@@ -0,0 +1,6 @@
+// import 'classlist.js';  // Run `npm install --save classlist.js`.
+
+// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+
+import 'zone.js/dist/zone';  // Included with Angular CLI.
+
